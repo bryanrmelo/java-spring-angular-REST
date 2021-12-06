210a125c1d4fe689284a9a4621a320b83b727254
Bryan Ribeiro Melo <bryanrmelo@gmail.com>
2021-12-06 13:47:45 -0300

    Routing

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MostrarCargoPorIdComponent } from './mostrar-cargo-por-id/mostrar-cargo-por-id.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MostrarCargoPorIdComponent
   ],
   imports: [
     BrowserModule,
