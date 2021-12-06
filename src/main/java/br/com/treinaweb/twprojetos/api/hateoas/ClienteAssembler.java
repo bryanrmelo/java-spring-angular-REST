0be4b0998da09dbd27e85cabff2b65b979214072
Bryan Ribeiro Melo <bryanrmelo@gmail.com>
2021-12-06 16:18:51 -0300

    Listar funcionarios

--- src/main/java/br/com/treinaweb/twprojetos/api/hateoas/ClienteAssembler.java
+++ src/main/java/br/com/treinaweb/twprojetos/api/hateoas/ClienteAssembler.java
@@ -19,38 +19,38 @@ public class ClienteAssembler implements SimpleRepresentationModelAssembler<Clie
     public void addLinks(EntityModel<Cliente> resource) {
         Long id = resource.getContent().getId();
 
-            Link selfLink = linkTo(methodOn(ClienteControleApi.class).buscarPorId(id))
-            .withSelfRel()
-            .withType("GET");
-            
-            Link editarLink = linkTo(methodOn(ClienteControleApi.class).atualizar(null, id))
-            .withSelfRel()
-            .withType("PUT");
+        Link selfLink = linkTo(methodOn(ClienteControleApi.class).buscarPorId(id))
+                .withSelfRel()
+                .withType("GET");
 
-            Link excluirLink = linkTo(methodOn(ClienteControleApi.class).excluirPorId(id))
-            .withSelfRel()
-            .withType("DELETE");
+        Link editarLink = linkTo(methodOn(ClienteControleApi.class).atualizar(null, id))
+                .withSelfRel()
+                .withType("PUT");
 
-            Link projetosLink = linkTo(methodOn(ClienteControleApi.class).buscarProjetos(id))
-            .withSelfRel()
-            .withType("GET");
+        Link excluirLink = linkTo(methodOn(ClienteControleApi.class).excluirPorId(id))
+                .withSelfRel()
+                .withType("DELETE");
 
-            resource.add(selfLink, editarLink, excluirLink, projetosLink);
-        
+        Link projetosLink = linkTo(methodOn(ClienteControleApi.class).buscarProjetos(id))
+                .withSelfRel()
+                .withType("GET");
+
+        resource.add(selfLink, editarLink, excluirLink, projetosLink);
+
     }
 
     @Override
     public void addLinks(CollectionModel<EntityModel<Cliente>> resources) {
         Link cadastroLink = linkTo(methodOn(ClienteControleApi.class).cadastrar(null))
-        .withSelfRel()
-        .withType("POST");
+                .withSelfRel()
+                .withType("POST");
 
         Link selfLink = linkTo(methodOn(ClienteControleApi.class).buscarTodos(null))
-        .withSelfRel()
-        .withType("GET");
-        
+                .withSelfRel()
+                .withType("GET");
+
         resources.add(selfLink, cadastroLink);
-        
+
     }
-    
+
 }
